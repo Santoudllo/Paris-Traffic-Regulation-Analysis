781b39aa26f70f660d1ae0c5b006908b9d615df5
santoudllo <mamadoualimou.diallo@ynov.com>
2024-04-15 17:47:58 +0200

    modification_sur l'affihage Map

--- Processus_ETL/routier.py
+++ Processus_ETL/routier.py
@@ -1,20 +1,17 @@
 import requests
 import pandas as pd
 
-url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/comptages-routiers-permanents/records?limit=100"
+url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/comptages-routiers-permanents/records?limit=-1"
 response = requests.get(url)
 
 if response.status_code == 200:
-   
     data = response.json()
     df = pd.json_normalize(data['results'])
     
-   
-    output_file = "/home/santoudllo/Desktop/DEEP_LEARNING/Paris-Traffic-Regulation-Analysis/data/output_data.csv"
-    
+    output_file = "../data/output_data.csv"
     
     df.to_csv(output_file, index=False)
     
-    print("Les données ont été exportées avec succès vers:", output_file)
+    print("Les données ont été exportées avec succès vers :", output_file)
 else:
-    print("Erreur lors de la récupération des données:", response.status_code)
+    print("Erreur lors de la récupération des données :", response.status_code)
--- visualisation_donnees_brutes/visualisation_Streamlit.py
+++ visualisation_donnees_brutes/visualisation_Streamlit.py
@@ -1,42 +1,122 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+import folium
+from streamlit_folium import folium_static
+from geopy.geocoders import Nominatim
+import openrouteservice
 
-@st.cache_data
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
+st.set_page_config(layout="wide")
 
-# Chemin vers le fichier CSV
-file_path = "/home/santoudllo/Desktop/DEEP_LEARNING/Paris-Traffic-Regulation-Analysis/data/output_data.csv"
+file_path = "../data/comptages-routiers.csv"  # Chemin de votre fichier CSV
 
-data = load_data(file_path)
+if os.path.exists(file_path):
+    df = pd.read_csv(file_path)
+    st.title("Comptage routier - Données trafic issues des capteurs permanents")
 
-st.write("<h1 style='text-align:center; font-weight:bold;'>Comptage routier - Données trafic issues des capteurs permanents</h1>", unsafe_allow_html=True)
+    choice = st.sidebar.selectbox("Menu", ['Charger les données', 'EDA', 'MAP'])
 
-st.write("<h2 style='text-align:center;'>Visualisation du débit et du taux d'occupation</h2>", unsafe_allow_html=True)
+    if choice == 'Charger les données':
+        file = st.file_uploader("Chargez vos données")
+        separator = st.radio("Si votre dataset ne s'affiche pas correctement, sélectionnez le bon séparateur",
+                             [",", ";"])
+        if file:
+            df = pd.read_csv(file, index_col=None, sep=separator)
+            df.to_csv(file_path, index=None)
+            if len(df.columns) >= 2:
+                st.success("Données chargées correctement, vous pouvez passer à l'analyse")
+            else:
+                st.error("Il semble que vous avez sélectionné le mauvais séparateur")
+            st.dataframe(df)
 
-st.write("<p style='text-align:center'><strong>Cette dataviz donne à voir la donnée brute telle qu'elle est publiée, ce n'est en aucun cas un tableau de bord caractérisant la circulation à Paris.</strong></p>", unsafe_allow_html=True)
+    elif choice == 'EDA':
+        st.header("Rapport d'analyse exploratoire des données")
+        st.write(df.describe())  # Afficher les statistiques descriptives
+        
+        # Afficher un histogramme pour une colonne spécifique
+        column_to_plot = st.selectbox("Sélectionnez une colonne pour afficher un histogramme", df.columns)
+        plt.figure(figsize=(10, 6))
+        sns.histplot(df[column_to_plot], kde=True)
+        plt.title(f"Histogramme de la colonne {column_to_plot}")
+        plt.xlabel(column_to_plot)
+        plt.ylabel("Fréquence")
+        st.pyplot(plt)
 
-if not data.empty:
-    st.write(data)
-    
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
-    
-                            # Visualisation du débit
-    ax1.set_title("Visualisation du débit")
-    sns.histplot(data['q'], bins=30, kde=True, ax=ax1)
-    ax1.set_xlabel('Débit')
-    ax1.set_ylabel('Fréquence')
-    
-                           # Visualisation du taux d'occupation
-    ax2.set_title("Visualisation du taux d'occupation")
-    sns.histplot(data['k'], bins=30, kde=True, ax=ax2)
-    ax2.set_xlabel('Taux d\'occupation')
-    ax2.set_ylabel('Fréquence')
-    
-    st.pyplot(fig)
+        # Afficher un diagramme en barres pour une colonne spécifique
+        column_to_plot_bar = st.selectbox("Sélectionnez une colonne pour afficher un diagramme en barres", df.columns)
+        plt.figure(figsize=(10, 6))
+        sns.countplot(data=df, x=column_to_plot_bar)
+        plt.title(f"Diagramme en barres de la colonne {column_to_plot_bar}")
+        plt.xlabel(column_to_plot_bar)
+        plt.ylabel("Fréquence")
+        st.pyplot(plt)
 
+    elif choice == 'MAP':
+        st.header("Carte des données géographiques")
+        
+        # Sélectionnez le service de géocodage
+        geocoder = st.selectbox("Sélectionnez un service de géocodage", ["Nominatim"])
+        if geocoder == "Nominatim":
+            geolocator = Nominatim(user_agent="my_geocoder")
+        else:
+            st.error("Service de géocodage non pris en charge")
+        
+        # Entrée des adresses de départ et d'arrivée
+        start_address = st.text_input("Adresse de départ")
+        end_address = st.text_input("Adresse d'arrivée")
+        
+        # Sélectionnez la colonne d'état de trafic
+        traffic_state_column = st.selectbox("Sélectionnez la colonne d'état de trafic", df.columns)
+        
+        # Sélectionnez l'état de trafic
+        selected_traffic_state = st.selectbox("Sélectionnez l'état de trafic", df[traffic_state_column].unique())
+        
+        # Si les adresses de départ et d'arrivée sont fournies, géocodez-les
+        if start_address and end_address:
+            start_location = geolocator.geocode(start_address)
+            end_location = geolocator.geocode(end_address)
+            
+            # Si les emplacements de départ et d'arrivée sont trouvés, affichez-les sur la carte
+            if start_location and end_location:
+                m = folium.Map(location=[start_location.latitude, start_location.longitude], zoom_start=12)
+                folium.Marker([start_location.latitude, start_location.longitude], popup="Départ").add_to(m)
+                folium.Marker([end_location.latitude, end_location.longitude], popup="Arrivée").add_to(m)
+                
+                # Filtrer les données en fonction de l'état de trafic sélectionné
+                filtered_df = df[df[traffic_state_column] == selected_traffic_state]
+                
+                # Ajouter des marqueurs de couleur différente en fonction de l'état de trafic
+                for index, row in filtered_df.iterrows():
+                    coordinates = str(row['geo_point_2d']).split(',') if isinstance(row['geo_point_2d'], str) else None
+                    if coordinates:
+                        if row['Etat trafic'] == "Saturé":
+                            color = 'red'
+                        elif row['Etat trafic'] == "Fluide":
+                            color = 'green'
+                        else:
+                            color = 'blue'
+                        folium.Marker([float(coordinates[0]), float(coordinates[1])], popup=row['Libelle'], icon=folium.Icon(color=color)).add_to(m)
+                
+                # Afficher la carte dans Streamlit
+                folium_static(m)
+            else:
+                st.error("Impossible de géocoder les adresses fournies")
+        
+        # Afficher les points géographiques à partir du jeu de données
+        st.subheader("Points géographiques à partir du jeu de données")
+        m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)  # Coordonnées pour Paris
+        
+        # Ajoutez des marqueurs pour chaque point géographique dans votre jeu de données
+        for index, row in df.iterrows():
+            coordinates = str(row['geo_point_2d']).split(',') if isinstance(row['geo_point_2d'], str) else None
+            if coordinates:
+                folium.Marker([float(coordinates[0]), float(coordinates[1])], popup=row['Libelle']).add_to(m)
+            else:
+                print(f"Coordonnées manquantes ou invalides pour l'index {index}")
+        
+        # Affichez la carte dans Streamlit
+        folium_static(m)
 else:
-    st.error("Aucune donnée n'est disponible.")
+    st.error("Le chemin spécifié pour le fichier n'existe pas.")
